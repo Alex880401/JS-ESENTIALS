8af12292be4b82ad8b0bb6211bf1018852c63089
Alex880401 <Derevyanko.Alexey88@gmail.com>
2025-07-26 00:08:40 +0300

    Made 1st part

---
+++ assets/JS/index.js
@@ -0,0 +1,54 @@
+// 1)
+
+/*const a = Number(prompt('введіть 1 множник:'));
+const b = Number(prompt('Введіть 2 множник:'));
+const mult = a * b;
+console.log(mult);
+/*console.log(typeof a);
+console.log(typeof b);
+console.log(typeof mult);*/
+
+// 2)
+
+/*const c = Number(prompt('введіть чисельник:'));
+const d = Number(prompt('Введіть знаменник:'));
+const fr = c / d;
+console.log(fr);*/
+
+// 3)
+
+/*const e = Number(prompt('введіть 1 доданок:'));
+const f = Number(prompt('Введіть 2 доданок:'));
+const sum = e + f;
+console.log(sum);*/
+
+// 4)
+
+/*const num = Number(11);
+console.log(num);
+const bln = Boolean(true);
+console.log(bln);
+const str1 = String('java script');
+console.log(str1);
+const str2 = String('100');
+console.log(str2);*/
+
+// 5)
+
+let num = 1;
+console.log(num);
+num = num +=11;
+console.log(num);
+num = num -=11;
+console.log(num);
+num = num *=11;
+console.log(num);
+num = num /=11;
+console.log(num);
+num = num +=1;
+console.log(num);
+num = num -=1;
+console.log(num);
+
+
+
