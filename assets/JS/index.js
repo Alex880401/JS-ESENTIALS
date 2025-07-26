921c081bafdfef58cbbf40f693a8cff29fc04961
Alex880401 <Derevyanko.Alexey88@gmail.com>
2025-07-27 01:45:23 +0300

    Finish work

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -2,42 +2,39 @@
 
 // 1)
 
-/*const a = Number(prompt('введіть 1 множник:'));
+const a = Number(prompt('введіть 1 множник:'));
 const b = Number(prompt('Введіть 2 множник:'));
 const mult = a * b;
 console.log(mult);
-/*console.log(typeof a);
-console.log(typeof b);
-console.log(typeof mult);*/
 
 // 2)
 
-/*const c = Number(prompt('введіть чисельник:'));
+const c = Number(prompt('введіть чисельник:'));
 const d = Number(prompt('Введіть знаменник:'));
 const fr = c / d;
-console.log(fr);*/
+console.log(fr);
 
 // 3)
 
-/*const e = Number(prompt('введіть 1 доданок:'));
+const e = Number(prompt('введіть 1 доданок:'));
 const f = Number(prompt('Введіть 2 доданок:'));
 const sum = e + f;
-console.log(sum);*/
+console.log(sum);
 
 // 4)
 
-/*const num = Number(11);
-console.log(num);
+const num1 = Number(11);
+console.log(num1);
 const bln = Boolean(true);
 console.log(bln);
 const str1 = String('java script');
 console.log(str1);
 const str2 = String('100');
-console.log(str2);*/
+console.log(str2);
 
 // 5)
 
-/*let num = 1;
+let num = 1;
 console.log(num);
 num = num +=11;
 console.log(num);
@@ -50,34 +47,46 @@ console.log(num);
 num = num +=1;
 console.log(num);
 num = num -=1;
-console.log(num);*/
+console.log(num);
 
 // Введення/виведення
 
 // 1)
 
-/*const a = Number(prompt ('Введіть число:'));
-const pow = Math.pow(a,2);
-console.log('Число', a ,'в 2 степені дорівнює:', pow);*/
+const aa = Number(prompt ('Введіть число:'));
+const pow = Math.pow(aa,2);
+console.log('Число', aa ,'в 2 степені дорівнює:', pow);
 
 // 2)
 
-/*const b = Number(prompt ('Введіть 1 число:'));
-const c = Number(prompt ('Введіть 2 число:'));
-const av = (b + c) / 2;
-console.log('Середнє арифметичне чисел', b ,'і', c, 'дорівнює:', av);*/
+const bb = Number(prompt ('Введіть 1 число:'));
+const cc = Number(prompt ('Введіть 2 число:'));
+const av = (bb + cc) / 2;
+console.log('Середнє арифметичне чисел', bb ,'і', cc, 'дорівнює:', av);
 
 // 3)
 
-/*const min = Number(prompt ('Введіть кількість хвилин:'));
+const min = Number(prompt ('Введіть кількість хвилин:'));
 const hrs = min / 60;
-console.log('Кількість годин в', min ,'хвилинах:', hrs);*/
+console.log('Кількість годин в', min ,'хвилинах:', hrs);
 
 // 4)
 
 const gretting = 'Hello, ';
 const userName = prompt("Введіть Ваше ім'я!");
 alert (gretting + userName);
+
+// 5)
+
+const firstName = (prompt("Введіть ім'я коримтувача"));
+const lastName = (prompt("Введіть прізвище користувача")); 
+const Email =  (prompt("Введіть Email"));
+
+document.writeln ("<h2>" + firstName + " " + lastName + "<h2/>");
+document.writeln ("<p>" + Email + "<p/>");
+
+
+
 
 
 
